62da334ffbee999ab2901e77c617baeb7794dcdd
육승준 <6seungjun@naver.com>
2021-07-10 00:19:29 +0900

    first day

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Movie App 2021
 
 React JS Fundamentals Course (2021 First)
+
+2021/07/10
+- 2 JSX까지 수강
+- 3 STATE들을 차례
--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Proptypes from 'prop-types';
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "bulgogi",
+    image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+    rating: 4
+  },
+  {
+    id: 3,
+    name: "kimbap",
+    image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+    rating: 3
+  },
+  {
+    id: 4,
+    name: "samgyetang",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 2
+  }
+]  
+
+function Food({ name, picture, rating }) {
+  return <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
+    </div>
+}
+
+Food.propTypes = {
+  name: Proptypes.string.isRequired,
+  picture: Proptypes.string.isRequired,
+  rating: Proptypes.number.isRequired
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map(dish => (
+        <Food 
+          key={dish.id} 
+          name={dish.name} 
+          picture={dish.image} 
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
